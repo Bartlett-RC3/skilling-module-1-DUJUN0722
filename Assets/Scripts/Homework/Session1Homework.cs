a2a1eb45939f88205c7a691e853b06cfa02d79b2
DUJUN0722 <44259644+DUJUN0722@users.noreply.github.com>
2018-10-25 13:47:04 +0100

    Session1Homework

--- Assets/Scripts/Homework/Session1Homework.cs
+++ Assets/Scripts/Homework/Session1Homework.cs
@@ -2,15 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Session1Homework : MonoBehaviour {
+public class Session1Homework : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        // HOMEWORK i, ii, iii
+        int myInt = 123;
+        float myFloat = 123.123f;
+        string myName = "JUN";
+
+        // HOMEWORK iv
+        Debug.Log(myInt);
+        Debug.Log(myFloat);
+        Debug.Log(myName);
+
+        // HOMEWORK v
+        int[] myArray = { 1, 2, 3, 4, 5 };
+        Debug.Log(myArray);
+
+        // HOMEWORK vi
+        List<double> myList = new List<double>();
+        myList.Add(1.11);
+        myList.Add(2.22);
+        Debug.Log(myList);
+
+        // HOMEWORK vii
+        Dictionary<string, string> myDictionary = new Dictionary<string, string>;
+        myDictionary.Add("sweet", "chocolate");
+        myDictionary.Add("sweet", "sugar");
+        myDictionary.Add("spicy", "chili");
+        myDictionary.Add("spicy", "garlic");
+
+        if (myDictionary.ContainsKey("sweet")) 
+        {
+            Debug.Log("The sweet food is" + myDictionary.Values);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
