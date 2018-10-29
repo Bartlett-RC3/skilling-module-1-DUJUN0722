2c3272fcfc1745814d4c5a456d4f20d77c54779a
DUJUN0722 <44259644+DUJUN0722@users.noreply.github.com>
2018-10-29 10:31:17 +0000

    Session 3 Homework

---
+++ Assets/Scripts/Homework/Session3Homework.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Session3Homework : MonoBehaviour {
+
+    
+    public float speed = 0.1f;
+    public Material color;
+
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        float offset = Time.time * speed;
+        Debug.Log(offset);
+        transform.Translate(offset, 0.0f, 0.0f);
+
+        if (Input.GetMouseButton(0))
+        {
+            color.color = new Color(Random.value, Random.value, Random.value);
+        }
+	}
+}
